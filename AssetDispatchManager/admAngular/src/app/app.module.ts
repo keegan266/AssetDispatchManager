49099b6d79bdd66c57069a741b3cc9e8927315fb
Keegan <keeganahart@gmail.com>
2020-03-13 00:48:55 -0500

    Worked on form

--- AssetDispatchManager/admAngular/src/app/app.module.ts
+++ AssetDispatchManager/admAngular/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { AppFormComponent } from './appForm/appForm.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    AppFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [LoginComponent]
+  bootstrap: [AppFormComponent]
 })
 export class AppModule { }
---
+++ AssetDispatchManager/admAngular/src/app/appForm/appForm.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-Form',
+    templateUrl: './appForm.component.html',
+    styleUrls: ['./appForm.component.css']
+})
+export class AppFormComponent {
+    title: 'Create Form';
+}
