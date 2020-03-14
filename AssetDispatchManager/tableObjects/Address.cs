049cfca384bc904ae359aa241e911a3df3459a78
Taylor G <taylorg09784@gmail.com>
2020-03-14 03:29:13 -0500

    Removed Razor
    Removed Razor and reworked entire project into Angular based instead of Razor. Reworked C# mysql objects to no longer be models but proper classes, started work on DataAdapter

---
+++ AssetDispatchManager/tableObjects/Address.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetDispatchManager.tableObjects
+{
+    public class Address
+    {
+        #region Private Address Variables
+        private int addressID;
+        private string buildingName;
+        private string addressLine1;
+        private string addressLine2;
+        private string city;
+        private string state;
+        private string Zipcode;
+        #endregion
+
+        #region Public Address GetterSetters
+        public int AddressID { get => addressID; set => addressID = value; }
+        public string BuildingName { get => buildingName; set => buildingName = value; }
+        public string AddressLine1 { get => addressLine1; set => addressLine1 = value; }
+        public string AddressLine2 { get => addressLine2; set => addressLine2 = value; }
+        public string City { get => city; set => city = value; }
+        public string State { get => state; set => state = value; }
+        public string Zipcode1 { get => Zipcode; set => Zipcode = value; }
+        #endregion
+
+        #region Public Address Constructors
+        public Address(int addressID, string buildingName, string addressLine1, string addressLine2, string city, string state, string zipcode1)
+        {
+            AddressID = addressID;
+            BuildingName = buildingName;
+            AddressLine1 = addressLine1;
+            AddressLine2 = addressLine2;
+            City = city;
+            State = state;
+            Zipcode1 = zipcode1;
+        }
+        #endregion
+    }
+}
---
+++ AssetDispatchManager/tableObjects/Tech.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetDispatchManager.tableObjects
+{
+    public class Tech
+    {
+        #region Private Tech Variables
+        private int techID;
+        private string firstName;
+        private string lastName;
+        private string jobRole;
+        private string email;
+        private string phoneNumber;
+        private string isWorking;
+        #endregion
+
+        #region Public Tech GetterSetters
+        public int TechID { get => techID; set => techID = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string JobRole { get => jobRole; set => jobRole = value; }
+        public string Email { get => email; set => email = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string IsWorking { get => isWorking; set => isWorking = value; }
+        #endregion
+
+        #region Public Tech Constructors
+        public Tech(int techID, string firstName, string lastName, string jobRole, string email, string phoneNumber, string isWorking)
+        {
+            TechID = techID;
+            FirstName = firstName;
+            LastName = lastName;
+            JobRole = jobRole;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            IsWorking = isWorking;
+        }
+        #endregion
+    }
+}
