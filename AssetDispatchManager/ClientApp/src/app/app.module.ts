0168ee7041ee671190b7805d58efddfd9e22b812
Keegan Hart <keeganahart@gmail.com>
2020-05-07 15:55:40 -0500

    Working on routing

--- AssetDispatchManager/ClientApp/src/app/app.module.ts
+++ AssetDispatchManager/ClientApp/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { CalendarComponent } from './calendar/calendar.component';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { FullCalendarModule } from '@fullcalendar/angular';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -24,9 +26,14 @@ import { FullCalendarModule } from '@fullcalendar/angular';
       provide: DateAdapter,
       useFactory: adapterFactory,
     }),
-    FullCalendarModule
+    FullCalendarModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent},
+      {path: 'calendar', component: CalendarComponent}
+    ])
   ],
   providers: [],
-  bootstrap: [CalendarComponent]
+  bootstrap: [LoginComponent]
 })
 export class AppModule { }
